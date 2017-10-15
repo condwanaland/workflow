be2a2dce58fdbb7eb884c9d503ae8afe66376d51
Conor Neilson <condwanaland@gmail.com>
2017-10-15 19:44:26 +1300

    first function

---
+++ R/make_filename.R
@@ -0,0 +1,12 @@
+make_filename <- function(file="", path="", add.terminal = F) {
+  if(path != "") {
+    plc <- substring(path, nchar(path))
+    if(!(plc == "\\" | plc == "/")) path <- paste(path, "\\", sep = "")
+  }
+  filename <- paste(path, file, sep = "")
+  if(add.terminal==T) {
+    plc <- substring(filename, nchar(filename))
+    if(!(plc == "\\" | plc == "/")) filename <- paste(filename, "\\", sep = "")
+  }
+  return(filename)
+}
