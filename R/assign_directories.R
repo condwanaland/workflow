f6f8d44eabc391609cddd1b0cc6b10a3e0972b93
Conor Neilson <condwanaland@gmail.com>
2017-10-16 19:16:14 +1300

    add function

---
+++ R/assign_directories.R
@@ -0,0 +1,17 @@
+assign.directories<-function(base="") {
+  DIR<-list()
+  # base directory
+  DIR[["Base"]]<-base
+  # R directory
+  DIR[["R"]]<-make.filename("R",DIR[["Base"]],T)
+  # R functions
+  DIR[["Functions"]]<-make.filename("Functions",DIR[["R"]],T)
+  # Scripts
+  DIR[["Scripts"]]<-make.filename("Scripts", DIR[["R"]],T)
+  # Raw Data
+  DIR[["Data"]]<-make.filename("Data",DIR[["Base"]],T)
+  # Figures
+  DIR[["Figures"]]<-make.filename("Figures",DIR[["Base"]],T)
+
+  return(DIR)
+}
