f89bdfa5ed9e1dfb9b67365e51ce23500b6e4a5b
Conor Neilson <condwanaland@gmail.com>
2017-12-12 18:46:31 +1300

    Update to use here::here

--- templates/00-init.R
+++ templates/00-init.R
@@ -1,2 +1,1 @@
-current_dir <- getwd() # Note, this will only work if you are in a Rstudio project. Otherwise, set your working directory first
-DIR <- assign_directories(base = current_dir)
+DIR <- assign_directories(base = here::here())
